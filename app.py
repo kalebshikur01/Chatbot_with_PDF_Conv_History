45286f37472fbebd9851c505b6fe3dfd158395ca
Kaleb Shikur <kalebm1sales@gmail.com>
2025-05-09 21:48:31 -0400

    new RAG application script added

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-print("hello world")
+# import all the neccessary modules
+# enviroment variable, os, groq, chatprompt template, create_stuff document chain, create retrieval chain,
 
-print("Hello Hello")
+# load enviroment variables, 
+
+
+# create groq model
+
+# create chatprompt template
+
+# vector embedding function 
+# st.seesion_state.embedding, loader, docs, text_splitter, final_docoment(split_documents()), vectors=FAISS(final_docuemnts, embeddings)
+# No need to return the final vectors since it is saved in the session.
+
