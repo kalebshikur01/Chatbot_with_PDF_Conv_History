ab9fbea11b6d15cee03360d8b60407fcf6ade631
Kaleb Shikur <kalebm1sales@gmail.com>
2025-05-20 21:10:03 -0400

    Several bugs fixed and the application is running smothly

---
+++ README.md
@@ -0,0 +1,3 @@
+To run the streamlit app use the command
+
+streamlit run app.py
--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 import os
 import time
 from langchain_groq import ChatGroq
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -21,6 +21,7 @@ load_dotenv()
 
 os.environ['GROQ_API_KEY']=os.getenv('GROQ_API_KEY')
 groq_api_key=os.getenv('GROQ_API_KEY')
+os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
 
 # create groq model
 llm = ChatGroq(model="Llama3-8b-8192")
@@ -42,13 +43,13 @@ User question: {input}
 # No need to return the final vectors since it is saved in the session.
 
 def create_vector_embedding():
-    if "vector" not in st.session_state:
-        st.session_state.embeddings=OllamaEmbeddings()
-        st.session_state.loader = PyMuPDFLoader('pdf_repo')
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("pdf_repo")  ## creating a document loader to load all the documents in the provided directory
         st.session_state.docs = st.session_state.loader.load()
         st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        st.session_state.final_document=st.session_state.text_splitter.split_documents(st.session_state.docs)
-        st.session_state.vectors=FAISS(st.session_state.final_documents, st.session_state.embeddings)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
 
 
 # display text input requesting for query
@@ -56,7 +57,7 @@ user_prompt=st.text_input("Enter your query from the research papaer")
 #add button to create/ add embedding to the session.
 if st.button("Document Embedding"):
     create_vector_embedding()
-    st.write("Vector embedding completed!")
+    st.write("Vector DB created!")
 # After embedding completes and user provides input
 
 if user_prompt:
@@ -65,7 +66,12 @@ if user_prompt:
     retrieval_chain=create_retrieval_chain(retriever, document_chain)
 
     start=time.process_time()
-    response= retrieval_chain.invoke(user_prompt)
+    response= retrieval_chain.invoke({'input':user_prompt})
     print(f"Process completed Response time: {start-time.process_time()}")
 
     st.write(response['answer'])
+
+    with st.expander("Document similarity search"):
+        for i, doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('--------------------')
